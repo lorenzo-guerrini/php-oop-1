fffffaf1dd2e9d866ad48c16c7a8667f7e06af88
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-28 09:09:54 +0100

    cambiato titolo pagina

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $peppa = new Movie("Peppa Pig", ["cartoon"]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Movies</title>
 </head>
 
 <body>
