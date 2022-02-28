2d7bc5f0b4c462162f5ba67b8841cb8ec00bf914
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-28 09:06:38 +0100

    Create istanze e html

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/classes/Movie.php';
+$amelie = new Movie("Le Fabuleux Destin d'Amélie Poulain", ["comedy", "romance"]);
+$peppa = new Movie("Peppa Pig", ["cartoon"]);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo $amelie->getInfo();
+    echo $peppa->getInfo();
+    ?>
+</body>
+</html>
